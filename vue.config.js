5cd04823f9397300b8f5653d4b66e1c66d1cf269
Caleb <choover11@gmail.com>
2020-03-28 16:47:22 -0700

    got too excited, forgot some manifest stuff

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'Hacker News',
+    themeColor: '#AA0000',
+    msTileColor: '#EFEFEF',
+    manifestOptions: {
+      background_color: '#EFEFEF'
+    }
+  }
+}
